70a4a4d09db335abc2eeae48ec8801f9837ae4f5
STACY <stacy.imagines888@passinbox.com>
2026-01-05 18:56:59 +0100

    add PokemonCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import "./App.css";
 
 import MyTitle from "./components/MyTitle";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
   return (
     <section>
       <MyTitle />
+      <PokemonCard />
     </section>
+
+
   );
 }
-
 
 export default App;
---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,9 @@
+function PokemonCard() {
+    return (
+        <div className="pokemon-card">
+            <img src="/src/assets/Images/151.png" alt="MewImg" />
+            <h2>Mew</h2>
+        </div>
+    );
+}
+export default PokemonCard;
