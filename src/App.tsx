1edf76c74851872c1de3886a574c45613a4d8f79
STACY <stacy.imagines888@passinbox.com>
2026-01-05 16:34:39 +0100

    My title location modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 
+import MyTitle from "./components/MyTitle";
+
 function App() {
   return (
     <section>
-      <h1>Hello React</h1>
+      <MyTitle />
     </section>
   );
 }
+
 
 export default App;
