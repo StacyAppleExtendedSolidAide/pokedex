b185925a9f1d22ff1580418eee0f4f22751f4c92
STACY <stacy.imagines888@passinbox.com>
2026-01-13 09:44:37 +0100

    Modified PokemonCard.tsx

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,19 @@
+const pokemonList = [
+    {
+        name: "Mew",
+        image: "/src/assets/Images/151.png",
+    },
+    {
+        name: "Sylveon",
+    }
+];
+
 function PokemonCard() {
+    const pokemon = pokemonList[0];
     return (
         <div className="pokemon-card">
-            <img src="/src/assets/Images/151.png" alt="MewImg" />
-            <h2>Mew</h2>
+            <img src={pokemon.image} alt={pokemon.name} />
+            <h2>{pokemon.name}</h2>
         </div>
     );
 }
